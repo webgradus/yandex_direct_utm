c3c55fbd39ab5ed5642c29ed783bdf5348ea6514
Viktor <optimizmforever@gmail.com>
2014-02-03 14:11:19 +0400

    change router

--- lib/yandex_direct_utm_controller.rb
+++ lib/yandex_direct_utm_controller.rb
@@ -2,16 +2,14 @@ require 'nyny'
 require 'yandex_direct_utm'
 require 'tilt'
 
-module YandexDirectUtm
-    class Router < NYNY::App
+class YandexDirectRouter < NYNY::App
 
-        YandexDirectUtm::Logger.config(:searchable_routes).each do |route|
-            get route do
-                params[:page] = route
-                YandexDirectUtm::Logger.write_attrs(params) unless params.empty?
-                Tilt.new("app/views/#{route.gsub('/','')}.html.erb").render
-            end
+    YandexDirectUtm::Logger.config(:searchable_routes).each do |route|
+        get route do
+            params[:page] = route
+            YandexDirectUtm::Logger.write_attrs(params) unless params.empty?
+            Tilt.new("app/views/#{route.gsub('/','')}.html.erb").render
         end
-
     end
+
 end
