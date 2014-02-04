b331dcffbbf27ec15cb183f0ab3586595b993825
Viktor <optimizmforever@gmail.com>
2014-02-04 13:43:45 +0400

    refactor

---
+++ lib/yandex_direct_utm/yandex_direct_utm_config.rb
@@ -0,0 +1,22 @@
+
+module YandexDirectUtm
+    class Config
+
+        @config={
+            searchable_params: [:utm_source,:utm_medium,:utm_campaign,:utm_content,:utm_type,:utm_source,:utm_added,:utm_block,:utm_position,:utm_keyword,:page],
+            searchable_routes: ['/promo'],
+            txt_log: 'TXTLOG.txt',
+            csv_log: 'CSVLOG.csv'
+        }
+
+        def self.config
+            @config
+        end
+
+        def self.keys
+            config.keys
+        end
+
+    end
+
+end
---
+++ lib/yandex_direct_utm/yandex_direct_utm_controller.rb
@@ -0,0 +1,17 @@
+require 'nyny'
+require 'yandex_direct_utm_logger'
+require 'tilt'
+
+module YandexDirectUtm
+    class Router < NYNY::App
+
+        YandexDirectUtm::Logger.config(:searchable_routes).each do |route|
+            get route do
+                params[:page] = route
+                YandexDirectUtm::Logger.write_attrs(params) unless params.empty?
+                Tilt.new("app/views/#{route.gsub('/','')}.html.erb").render
+            end
+        end
+
+    end
+end
