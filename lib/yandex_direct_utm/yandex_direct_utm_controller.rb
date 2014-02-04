52b848fbb2da464cf605ea1714da804692e5652b
Viktor <optimizmforever@gmail.com>
2014-02-04 16:21:13 +0400

    sintra

--- lib/yandex_direct_utm/yandex_direct_utm_controller.rb
+++ lib/yandex_direct_utm/yandex_direct_utm_controller.rb
@@ -1,15 +1,15 @@
-require 'nyny'
+require 'sinatra'
 require 'yandex_direct_utm/yandex_direct_utm_logger'
 require 'tilt'
 
 module YandexDirectUtm
-    class Router < NYNY::App
+    class Router < Synatra::Base
 
         YandexDirectUtm::Logger.config(:searchable_routes).each do |route|
             get route do
                 params[:page] = route
                 YandexDirectUtm::Logger.write_attrs(params) unless params.empty?
-                render "app/views/yandex_direct/#{route.gsub('/','')}.html.erb"
+                redirect_to route
             end
         end
 
