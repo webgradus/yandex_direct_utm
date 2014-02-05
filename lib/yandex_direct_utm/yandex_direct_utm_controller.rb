0ed217e554fce52059f137860d8bdd218a2e0067
Viktor <optimizmforever@gmail.com>
2014-02-05 10:11:16 +0400

    add sintra-contrib

--- lib/yandex_direct_utm/yandex_direct_utm_controller.rb
+++ lib/yandex_direct_utm/yandex_direct_utm_controller.rb
@@ -1,14 +1,19 @@
-require 'sinatra'
+require "sinatra/base"
+require "sinatra/namespace"
 require 'yandex_direct_utm/yandex_direct_utm_logger'
 
 module YandexDirectUtm
-    class Router < Synatra::Base
+    class Router < Sinatra::Base
 
-        YandexDirectUtm::Logger.config(:searchable_routes).each do |route|
-            get route do
-                params[:page] = route
-                YandexDirectUtm::Logger.write_attrs(params) unless params.empty?
-                redirect_to route
+        register Sinatra::Namespace
+
+        namespace '/api' do
+            YandexDirectUtm::Logger.config(:searchable_routes).each do |route|
+                get route do
+                    params[:page] = route
+                    YandexDirectUtm::Logger.write_attrs(params) unless params.empty?
+                    redirect to(route)
+                end
             end
         end
 
--- yandex_direct_utm.gemspec
+++ yandex_direct_utm.gemspec
@@ -21,5 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_dependency "sinatra"
+  spec.add_dependency "sinatra-contrib"
+  spec.add_dependency 'tilt', '~> 1.4.1'
 
 end
