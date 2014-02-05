292a1c10e0380a988450de0b9e09cdc36f39b012
Viktor <optimizmforever@gmail.com>
2014-02-05 11:19:05 +0400

    chanage default pages

--- lib/yandex_direct_utm/yandex_direct_utm_config.rb
+++ lib/yandex_direct_utm/yandex_direct_utm_config.rb
@@ -4,7 +4,7 @@ module YandexDirectUtm
 
         @config={
             searchable_params: [:utm_source,:utm_medium,:utm_campaign,:utm_content,:utm_type,:utm_source,:utm_added,:utm_block,:utm_position,:utm_keyword,:page],
-            searchable_routes: ['/promo'],
+            searchable_routes: ['promo','contacts'],
             txt_log: 'TXTLOG.txt',
             csv_log: 'CSVLOG.csv'
         }
