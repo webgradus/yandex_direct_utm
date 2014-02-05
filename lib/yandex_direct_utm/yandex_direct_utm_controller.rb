ff67384db5c8824d4b80e679fa8439c6ff102da4
Viktor <optimizmforever@gmail.com>
2014-02-05 10:49:23 +0400

    some fix

--- lib/yandex_direct_utm/yandex_direct_utm_controller.rb
+++ lib/yandex_direct_utm/yandex_direct_utm_controller.rb
@@ -5,17 +5,12 @@ require 'yandex_direct_utm/yandex_direct_utm_logger'
 module YandexDirectUtm
     class Router < Sinatra::Base
 
-        register Sinatra::Namespace
-
-        namespace '/api' do
-            YandexDirectUtm::Logger.config(:searchable_routes).each do |route|
-                get route do
-                    params[:page] = route
-                    YandexDirectUtm::Logger.write_attrs(params) unless params.empty?
-                    redirect to(route)
-                end
+        YandexDirectUtm::Logger.config(:searchable_routes).each do |route|
+            get '/service/#{route}' do
+                params[:page] = route
+                YandexDirectUtm::Logger.write_attrs(params) unless params.empty?
+                redirect to(route)
             end
         end
-
     end
 end
